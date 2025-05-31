4f174ca05b53d71160de2055dba9ccd797dbd0cc
artem.denisov <artem.denisov@devexpress.com>
2025-06-01 00:01:02 +0400

    add script to extract good folder names

---
+++ extractNames.py
@@ -0,0 +1,14 @@
+import os
+from colored import Fore, Back, Style
+
+good_folder = 'audio/GOOD_AUDIO'
+os.chdir(good_folder)
+
+folders = os.listdir()
+
+with open('goodFoldersNames.txt', 'w') as file:
+    print(f"{Fore.blue}The file [goodFoldersNames.txt] is created and open to write.{Style.reset}")
+    for folder in sorted(folders):
+        file.write(f"{folder}\n")
+    print(f"{Fore.blue}All names are writen to file.{Style.reset}")
+        
