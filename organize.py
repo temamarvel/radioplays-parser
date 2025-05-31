96ce441eaa218eedc272118891feeaa73b5919d1
artem.denisov <artem.denisov@devexpress.com>
2025-05-31 23:09:46 +0400

    update organize script to organize good folders in special folder

--- organize.py
+++ organize.py
@@ -1,5 +1,15 @@
+import shutil
+from colored import Fore, Back, Style
+
 from verify import get_good_folders
 
-folders = get_good_folders()
+good_folders = get_good_folders()
 
-print(folders)
+print(good_folders)
+
+for folder in good_folders:
+    try:
+        shutil.move(f"{folder}", f"GOOD_AUDIO/{folder}")
+        print(f"{Fore.green}Done! The directory [{folder}] was moved to [GOOD_AUDIO/{folder}].{Style.reset}")
+    except Exception as error:
+        print(f"{Fore.red}Error! The directory [{folder}] wasn't moved.{Style.reset}")
