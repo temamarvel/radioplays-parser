870c317f5bf1d1dd07f12c2366422c4fe1f9b7b4
temamarvel <tema.denisoff@gmail.com>
2025-06-04 10:46:01 +0400

    refactoring

--- extractNames.py
+++ extractNames.py
@@ -1,13 +1,14 @@
 import os
 from colored import Fore, Back, Style
 
-good_folder = 'audio/!GOOD_AUDIO'
-os.chdir(good_folder)
+GOOD_FOLDER_PATH = "audio/!GOOD_AUDIO"
+OUTPUT_FILE_NAME = "goodFoldersNames.txt"
+os.chdir(GOOD_FOLDER_PATH)
 
 folders = os.listdir()
 
-with open('goodFoldersNames.txt', 'w') as file:
-    print(f"{Fore.blue}The file [goodFoldersNames.txt] is created and open to write.{Style.reset}")
+with open(OUTPUT_FILE_NAME, 'w') as file:
+    print(f"{Fore.blue}The file [{OUTPUT_FILE_NAME}] is created and open to write.{Style.reset}")
     for folder in sorted(folders):
         file.write(f"{folder}\n")
     print(f"{Fore.blue}All names are writen to file.{Style.reset}")
