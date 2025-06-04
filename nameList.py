4341719412f857265151341cb8e7f59642fc7386
temamarvel <tema.denisoff@gmail.com>
2025-06-04 10:47:09 +0400

    cleanup

--- nameList.py
+++ nameList.py
@@ -1,20 +1,17 @@
 import os
 from colored import Fore, Back, Style
 
-good_folder = 'audio/!GOOD_AUDIO'
-os.chdir(good_folder)
+GOOD_FOLDER_PATH = 'audio/!GOOD_AUDIO'
+os.chdir(GOOD_FOLDER_PATH)
 
 folders = os.listdir()
 
 file_names = []
 
 for folder in folders:
-    # print(f"{Fore.blue}Folder {folder}{Style.reset}")
     if not os.path.isdir(folder):
         continue
-
     file_names.append(os.listdir(folder))
-    # print(f"\t{os.listdir(folder)}")
 
 for name in sorted(file_names):
     print(name)
