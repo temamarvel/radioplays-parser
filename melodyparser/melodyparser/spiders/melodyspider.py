65585ecf83bf69939e8aeccece28a714e904c0ee
temamarvel <tema.denisoff@gmail.com>
2025-08-04 23:44:41 +0400

    fix

--- melodyparser/melodyparser/spiders/melodyspider.py
+++ melodyparser/melodyparser/spiders/melodyspider.py
@@ -8,15 +8,15 @@ class MelodyspiderSpider(scrapy.Spider):
 
     def parse(self, response):
         # 1. Находим карточки
-        cards = response.css("div.entity-snippet a::attr(href)").getall()
+        cards = response.css("div.entity-snippet a::attr(href)").get()
         for card_url in cards:
             full_url = response.urljoin(card_url)
             yield scrapy.Request(full_url, callback=self.parse_detail)
 
         # 2. Переход на следующую страницу
-        next_page = response.css("liscrapy crawl example -o data.json.pagination__item_type_next a::attr(href)").get()
-        if next_page:
-            yield response.follow(next_page, callback=self.parse)
+        # next_page = response.css("li.pagination__item_type_next a::attr(href)").get()
+        # if next_page:
+        #     yield response.follow(next_page, callback=self.parse)
 
     def parse_detail(self, response):
         # Парсим детальную информацию
