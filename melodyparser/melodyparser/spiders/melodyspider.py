9f5f40e86eb3dd36b3e9aa0c5077d4a072a1ecb2
temamarvel <tema.denisoff@gmail.com>
2025-08-05 00:01:07 +0400

    play with parser

--- melodyparser/melodyparser/spiders/melodyspider.py
+++ melodyparser/melodyparser/spiders/melodyspider.py
@@ -9,9 +9,15 @@ class MelodyspiderSpider(scrapy.Spider):
     def parse(self, response):
         # 1. Находим карточки
         cards = response.css("div.entity-snippet a::attr(href)").get()
-        for card_url in cards:
-            full_url = response.urljoin(card_url)
-            yield scrapy.Request(full_url, callback=self.parse_detail)
+        print(f"!!cards!!={cards}")
+        # for card_url in cards:
+        #     full_url = response.urljoin(card_url)
+        #     print(f"!!full_url!!={full_url}")
+        #     yield scrapy.Request(full_url, callback=self.parse_detail)
+
+        full_url = response.urljoin(cards)
+        print(f"!!full_url!!={full_url}")
+        yield scrapy.Request(full_url, callback=self.parse_detail)
 
         # 2. Переход на следующую страницу
         # next_page = response.css("li.pagination__item_type_next a::attr(href)").get()
@@ -22,7 +28,7 @@ class MelodyspiderSpider(scrapy.Spider):
         # Парсим детальную информацию
         yield {
             "title": response.css("h1::text").get(),
-            "text": response.css("div.content p::text").getall(),
-            "images": response.css("img::attr(src)").getall(),
-            "links": response.css("a::attr(href)").getall()
+            # "text": response.css("div.content p::text").getall(),
+            # "images": response.css("img::attr(src)").getall(),
+            # "links": response.css("a::attr(href)").getall()
         }
